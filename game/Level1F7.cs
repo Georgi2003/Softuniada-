eac10e4f7274e0bf8785e71633e6f35fad97d449
Georgi2003 <georgidim30@gmail.com>
2020-11-09 13:35:57 +0200

    Create game

---
+++ game/Level1F7.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Level1F7 : Information
+    {
+        private int boiler;
+        private string inform;
+
+        public Level1F7(string inf, string pS, string info, int boil) : base(inf, pS)                       
+        {
+            base.typeInformation = inf;
+            this.boiler = boil;
+            this.inform = info;
+        }
+
+        public int getBoiler()
+        {
+            return this.boiler;
+        }
+
+        public void setBoiler(int h)
+        {
+            if (h > 0)
+            {
+                this.boiler = h;
+            }
+            else
+            {
+                this.boiler = 0;
+            }
+        }
+
+        public string getInform()
+        {
+            return this.inform;
+        }
+
+        public void setInform(string i)
+        {
+            this.inform = i;
+        }
+
+        public override string ToString()
+        {
+            string res = "Брои уреди за захранване: " + this.boiler + "\r\n" + "Информация: " + "\r\n" + this.inform;
+            return res;
+        }
+    }
+}
---
+++ game/Level1F9.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class Level1F9 : Information
+    {
+        private int kooker;
+        private string inform;
+
+        public Level1F9(string inf, string pS, string info) : base(inf, pS)                       
+        {
+            base.typeInformation = inf;
+            this.kooker = 1;
+            this.inform = info;
+        }
+
+        public string Car()
+        {
+            string res = "Играчът може да си избере кола!!!";
+            return res;
+        }
+
+        public int getHeating()
+        {
+            return this.kooker;
+        }
+
+        public void setHeating(int h)
+        {
+            if (h > 0)
+            {
+                this.kooker = h;
+            }
+        }
+
+        public string getInform()
+        {
+            return this.inform;
+        }
+
+        public void setInform(string i)
+        {
+            this.inform = i;
+        }
+
+        public override string ToString()
+        {
+            string res = "Брои готварски печки за захранване: " + this.kooker + "\r\n" + "Информация: " + "\r\n" + this.inform;
+            return res;
+        }
+    }
+}
